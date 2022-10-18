1ddec8596e7a2ef2984455672f1a115021327a31
minghunt <114232628+minghunt@users.noreply.github.com>
2022-10-18 18:39:18 +0700

    Add project files.

---
+++ OKXE/OKXE/Model/Exchange.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace OKXE.Model
+{
+    public class Exchange
+    {
+        private static Exchange _instance;
+        public static Exchange Data
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Exchange();
+                }
+                return _instance;
+            }
+        }
+
+        public string Buffer { get; set; }
+
+        public CollectionView MyCoView { get; set; }
+
+    }
+}
